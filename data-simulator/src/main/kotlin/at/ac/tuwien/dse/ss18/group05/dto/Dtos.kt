566046b8c71d91a0677de9cc4c04dfa4fce96783
Daniel Füvesi <e1326576@student.tuwien.ac.at>
2018-04-17 17:30:29 +0200

    add DTOs to data-simulator; update build files to support spring rest docs

--- data-simulator/src/main/kotlin/at/ac/tuwien/dse/ss18/group05/dto/Dtos.kt
+++ data-simulator/src/main/kotlin/at/ac/tuwien/dse/ss18/group05/dto/Dtos.kt
@@ -9,8 +9,63 @@ package at.ac.tuwien.dse.ss18.group05.dto
  * @version 1.0.0
  * @since 1.0.0
  */
+@Deprecated(
+    "This is just dummy notification for API test, use VehicleNotification instead.",
+    level = DeprecationLevel.WARNING
+)
 data class Notification(
     val id: String,
     val concernedVehicles: List<String>,
     val message: String
 )
+
+/***************************** OUTGOING *****************************/
+data class VehicleDataRecord(
+    val id: String?,
+    val timestamp: Long,
+    val metaData: MetaData,
+    val sensorInformation: SensorInformation,
+    val eventInformation: EventInformation
+)
+
+data class GpsLocation(val lat: Double, val lon: Double)
+
+data class ProximityInformation(
+    val distanceToVehicleFrontInCm: Double,
+    val distanceToVehicleBehindInCm: Double
+)
+
+data class SensorInformation(
+    val location: GpsLocation,
+    val proximityInformation: ProximityInformation,
+    val passengers: Int,
+    val speed: Double
+)
+
+data class MetaData(
+    val identificationNumber: String,
+    val model: String
+)
+
+enum class EventInformation {
+    NEAR_CRASH,
+    CRASH,
+    NONE
+}
+
+/***************************** INCOMING *****************************/
+data class VehicleNotification(
+    val id: String?,
+    val accidentId: String,
+    val timestamp: Long,
+    val location: GpsLocation,
+    val emergencyServiceStatus: EmergencyServiceStatus,
+    val specialWarning: Boolean?,
+    val targetSpeed: Double?
+)
+
+enum class EmergencyServiceStatus {
+    ARRIVED,
+    AREA_CLEARED,
+    UNKNOWN
+}
